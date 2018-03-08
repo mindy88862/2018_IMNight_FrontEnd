04e83ef7c1308dca5830b44779c3f0ab94dba28e
wendeehsu <30789244+wendeehsu@users.noreply.github.com>
2018-03-09 00:59:50 +0800

    finish ajax(bot img cannot show)

--- js/course.js
+++ js/course.js
@@ -1,68 +1,76 @@
-
-
 var resource = new Vue({
-	el:'.main',
-	data:{
-		classes:[
-			{
-				num:1,
-				name:'程式設計',
-				teacher:'小傑',
-				more:"大一必修",
-				imgUrl:'https://datasciencebe.files.wordpress.com/2015/01/coursera.png',
-				Info:"想當初就是這樣一路辛苦學會程式的啊！但現在回頭一看，一切都是值得的！",
-			},{
-				num:2,
-				name:'資料結構',
-				teacher:'坤叔',
-				more:"考古課",
-				imgUrl:'../img/bgRed.jpg',
-				Info:"blablabla",
-			},{
-				num:3,
-				name:'統計',
-				teacher:'小阿姨',
-				more:"很重",
-				imgUrl:'../img/bgRed.jpg',
-				Info:"to be continue",
-			},{
-				num:4,
-				name:'網路',
-				teacher:'松哥',
-				more:"大三必修",
-				imgUrl:'../img/bgRed.jpg',
-				Info:"to be continue",
-			}
-		]
-	},
-	methods: {
-		move: function(k) {
-			k = String(k);
-			/*initiate others*/
-			for(var i = 1;i <= this.classes.length; i++){
-				if(i == Number(k)){
-					$('#card'+k).toggleClass('lift');
-					$('#back'+k).toggleClass('transform-active');
-					$('#cb'+k).toggleClass('moveRight');
-				}else{
-					$('#card'+String(i)).removeClass('lift');
-					$('#back'+String(i)).removeClass('transform-active');
-					$('#cb'+String(i)).removeClass('moveRight');
+    el: '.main',
+    data: {
+		courseMorethanFour:false,
+        classes: []
+    },
+    methods: {
+        move: function(k) {
+            k = String(k);
+            /*initiate others*/
+            for (var i = 1; i <= this.classes.length; i++) {
+                if (i == Number(k)) {
+                    $('#card' + k).toggleClass('lift');
+                    $('#back' + k).toggleClass('transform-active');
+                    $('#cb' + k).toggleClass('moveRight');
+                } else {
+                    $('#card' + String(i)).removeClass('lift');
+                    $('#back' + String(i)).removeClass('transform-active');
+                    $('#cb' + String(i)).removeClass('moveRight');
+                }
+            }
+        },
+        crack: function(k,label) {
+            /* need to add 1 point*/
+            k = String(k);
+            $('#egg' + k).addClass('hide');
+
+            $('#eggDown' + k).removeClass('hide');
+            $('#eggDown' + k).addClass('downAnimate');
+
+            $('#eggUp' + k).removeClass('hide');
+            $('#eggUp' + k).addClass('upAnimate');
+			
+			$.ajax({
+				type: 'POST',
+				url: 'https://imnight2018backend.ntu.im/lottery/finish/',
+				xhrFields: {
+					withCredentials: true
+				},
+				data: JSON.stringify({"label":label}),
+				contentType: "application/json",
+				crossDomain: true,
+				beforeSend: function(request) {
+					var csrftoken = Cookies.get('csrftoken');
+					request.setRequestHeader("X-CSRFTOKEN", csrftoken);
+				},
+				success: function(data) {
+					console.log("label: "+label);
+				},
+				error: function(data) {
+					alert("fail POST" + data);
 				}
-			}		
+			});
+        },
+		showCourse: function(label,id) {
+			$.ajax({
+				url: 'https://imnight2018backend.ntu.im/sky/course/'+String(label)+'/',
+				type: 'GET',
+				xhrFields: {
+					withCredentials: true
+				},
+				success: function(data) {
+					console.log(data,id);
+					$('#des'+id).html(data[0].more);
+					$('#content'+id).html(data[0].content);
+				},
+				error: function(data) {
+					alert("fail showCourse" + data);
+				}
+			});
 		},
-		crack:function(k){
-			/* need to add 1 point*/
-			k = String(k);
-			$('#egg'+k).addClass('hide');
-			
-			$('#eggDown'+k).removeClass('hide');
-			$('#eggDown'+k).addClass('downAnimate');
-			
-			$('#eggUp'+k).removeClass('hide');
-			$('#eggUp'+k).addClass('upAnimate');
-		}
-	}
+		
+    }
 });
 
 $(function(){
@@ -75,3 +83,48 @@ $(function(){
         }
 	});	
 })
+
+$(function() {
+    $.ajax({
+        url: 'https://imnight2018backend.ntu.im/sky/list/courses/',
+        type: 'GET',
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function(data) {
+            //console.log(data);
+			if(data.length > 4){
+				resource.courseMorethanFour = true;
+				for (var i = 0; i < 4; i++) {
+					resource.classes.push(data[i]);
+				}
+			}else{
+				for (var i = 0; i < data.length; i++) {
+					resource.classes.push(data[i]);
+				}
+			}
+        },
+        error: function(data) {
+            alert("fail" + data);
+        }
+    });
+});
+
+function showMoreCourse(){
+    $.ajax({
+        url: 'https://imnight2018backend.ntu.im/sky/list/courses/',
+        type: 'GET',
+        xhrFields: {
+            withCredentials: true
+        },
+        success: function(data) {
+			for (var i = 5; i < data.length; i++) {
+				resource.classes.push(data[i]);
+			}
+        },
+        error: function(data) {
+            alert("fail" + data);
+        }
+    });
+	resource.courseMorethanFour = false;
+}
